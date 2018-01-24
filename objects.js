d97c9589ec5e668e501e3473a031dad72a9124df
Douglas Craig <dcraig112@gmail.com>
2018-01-24 23:54:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,6 @@
 var playlist = {beatles: "Hey Jude"}
 
 function updatePlaylist(playlist, artist, title) {
-  let o = {}
-  o[artist] = title
-  playlist.push(o)
+  playlist[artist] = title
   return playlist
 }
