c274e6c169d763dde3d63d1bf41bb8fcf807551d
Douglas Craig <dcraig112@gmail.com>
2018-01-24 23:48:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,1 @@
 var playlist = {beatles: "Hey Jude"}
-
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
-  return playlist
-}
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName]
-  return playlist
-}
